b762d6fb55b8d0a607f339644dee851b0f87c331
Luis9898 <luis.estrada@mavs.uta.edu>
2020-04-14 23:01:15 -0500

    Moved form and js code from query files to read_data.html and read.js

--- read.js
+++ read.js
@@ -1,5 +1,6 @@
 const UserList = document.querySelector('#user-list');
 
+//This function appends all of the queried information to HTML table
 function renderAccount(doc){
     let tr = document.createElement('tr');
     let id = document.createElement('td');
@@ -30,6 +31,7 @@ function renderAccount(doc){
     UserList.appendChild(tr);
 }
 
+//This function clears out HTML table of rows
 function delete_table(){
    var table = document.getElementById("user-list");
    while(table.rows.length > 1) {
@@ -37,6 +39,7 @@ function delete_table(){
   }
 }
 
+// This function provides the functionality for display all residents button
 function query_all(){
     delete_table();
     db.collection("Residents").get().then(function(querySnapshot) {
@@ -46,6 +49,7 @@ function query_all(){
     });
 }
 
+//This function queries based on resident ID
 function query_id(){
     delete_table();
     var form = document.getElementById("query_id");
@@ -58,7 +62,7 @@ function query_id(){
     });
     form.reset();
 }
-
+//This was the test function to query all Computer Science Majors
 function run(){
     delete_table();
     db.collection("Residents").where('major', '==', 'Computer Science').get().then(function(querySnapshot) {
@@ -66,4 +70,89 @@ function run(){
             renderAccount(doc);
         });
     });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Testing out new query code here */
+
+function single_query(collection,form,param){
+    var value = form.elements.namedItem(param).value;
+    var comp = "=="
+    if(value == ""){
+        comp = ">=";
+    }
+    console.log(param,comp,value);
+    var ids = new Array();
+    collection.where(param, comp, value)
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            //console.log(doc.data());
+            ids.push(doc.id);
+        });
+    })
+    return ids;
+}
+
+function query(){
+    var collection = db.collection("Residents");
+    var form = document.getElementById("query");
+
+    set1 = single_query(collection,form,"first");
+    set2 = single_query(collection,form,"last");
+    set3 = single_query(collection,form,"major");
+    set4 = single_query(collection,form,"year");
+    set5 = single_query(collection,form,"room_number");
+    set6 = single_query(collection,form,"room_letter");
+
+    setTimeout(() => {
+        x=set1.filter(value => -1 !== set2.indexOf(value));
+        x=x.filter(value => -1 !== set3.indexOf(value));
+        x=x.filter(value => -1 !== set4.indexOf(value));
+        x=x.filter(value => -1 !== set5.indexOf(value));
+        x=x.filter(value => -1 !== set6.indexOf(value));
+        setTimeout(print_set(x), 1000);
+    }, 2000);
+
+    form.reset();
+}
+
+// Trying to assign golbal variable that saves the item in the set, but not working
+var test;
+function print_set(set){
+    for (item in set){
+        console.log(set[item]);  
+        test = set[item]; 
+    }      
+}
+
+console.log(test);  
+
+
+
+//This function should print out the query based on the saved "test" variable, but not working
+function query_id_test(){
+    delete_table();
+    //var form = document.getElementById("query_id");
+    //var id = form.elements.namedItem("id").value;
+    //console.log(id);
+    db.collection("Residents").where(firebase.firestore.FieldPath.documentId(), '==', test).get().then(function(querySnapshot){
+        querySnapshot.forEach(function(doc) {
+        renderAccount(doc);
+        });
+    });
+    form.reset();
 }
